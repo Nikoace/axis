69c6355bd86920286362b88c9b7f5f9c69988faa
Kyo <32727925+Nikoace@users.noreply.github.com>
2019-01-16 00:28:10 +0800

    update: 更新工程结构

--- src/main/java/com/xu/axis/AxisApplication.java
+++ src/main/java/com/xu/axis/AxisApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AxisApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AxisApplication.class, args);
     }
